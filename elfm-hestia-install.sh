bebc0f98101004ce8532f5d85659aeec982d3202
Byosik <57840254+Byosik@users.noreply.github.com>
2020-06-11 16:21:15 +0200

    Create elfm-hestia-install.sh

---
+++ elfm-hestia-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function elfminstaller() { 
+echo "HestiaCP Installing ELFM...." 
+cd /usr/local/hestia/web/list
+wget https://raw.githubusercontent.com/vesta-cp/vestacp-file-manager/master/file%20manager.tar -O elfm.tar
+tar -xf elfm.tar
+rm -rf elfm.tar
+# Add the link to the panel.html file 
+if grep -q 'elfm' /usr/local/hestia/web/templates/admin/panel.html; then
+		echo 'Already there.'
+	else 
+sed -i '/<a alt="<?=__(\x27Notifications/i <div class="l-menu__item"><a href="/list/elfm/" alt="<?=__('elfm')?>" class="l-profile__logout"><i class="fas fa-folder-open"></i></a></div>' /usr/local/hestia/web/templates/admin/panel.html;
+fi
+echo "Done! Check HestiaCP!" 
+}
+elfminstaller
